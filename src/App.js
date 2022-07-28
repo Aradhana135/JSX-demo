4bf3e1599a3ed2397d2bf440263b1382ba194680
Aradhana135 <aradhana.kumari@creolestudios.com>
2022-07-28 18:01:11 +0530

    jsx-demo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ListOfThings from './Components/ListOfThings';
+
+function App() {
+  return (
+    <>
+    <ListOfThings/>
+    </>
+  );
+}
+
+export default App;
